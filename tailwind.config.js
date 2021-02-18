9f68d7f77ca5a8469fa230f01810753fee64c922
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2021-02-18 14:13:22 +0200

    extend tailwind color test

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
